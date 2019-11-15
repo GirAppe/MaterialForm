dee2008888e4216f4368f5adec44a1cf85eda963
Andrzej Michnia <amichnia@gmail.com>
2019-11-15 14:49:33 +0100

    Small pdate to contents for SwiftUI

--- SampleSwiftUI/ContentView.swift
+++ SampleSwiftUI/ContentView.swift
@@ -16,32 +16,32 @@ struct ContentView: View {
     @State var placeholder: String = "placeholder asdasd asdasd"
 
     var body: some View {
-        Form {
-            Section(header: Text("test")) {
-                MaterialTextField(title: "Test 1", text: $string) {
-                    $0.borderStyle = .bezel
-                    $0.textColor = .brown
-                    $0.font = .systemFont(ofSize: 16, weight: .semibold)
+        NavigationView {
+            Form {
+                Section(header: Text("test")) {
+                    MaterialTextField(title: "Test 1", text: $string) {
+                        $0.borderStyle = .bezel
+                        $0.textColor = .brown
+                        $0.font = .systemFont(ofSize: 16, weight: .semibold)
+                    }
+                    MaterialTextField(title: $placeholder, text: $string) {
+                        $0.textColor = .darkText
+                        $0.clearButtonMode = .whileEditing
+                        $0.borderStyle = .line
+                        $0.leftAccessory = .info(#imageLiteral(resourceName: "show").withRenderingMode(.alwaysTemplate))
+                    }
+                    MaterialTextField(title: "Long text for placeholder above:", text: $placeholder) {
+                        $0.textColor = .blue
+                        $0.clearButtonMode = .always
+                        $0.font = .systemFont(ofSize: 32, weight: .semibold)
+                    }
                 }
-                MaterialTextField(title: $placeholder, text: $string) {
-                    $0.textColor = .darkText
-                    $0.tintColor = .yellow
-                    $0.clearButtonMode = .whileEditing
-                    $0.borderStyle = .line
-                }
-                MaterialTextField(title: "Long text for placeholder above:", text: $placeholder) {
-                    $0.textColor = .blue
-                    $0.clearButtonMode = .always
-                    $0.font = .systemFont(ofSize: 32, weight: .semibold)
-                }
+                Spacer()
+                TextField("Test 2", text: $string)
             }
-            Spacer()
-            TextField("Test 2", text: $string)
+            .navigationBarTitle(Text("MaterialForm"))
+            .accentColor(.orange)
         }
-        .background(Color.red.opacity(0.2))
-        .accentColor(.orange)
-        .foregroundColor(.green)
-        .font(.system(size: 20, weight: .bold, design: .default))
     }
 }
 
